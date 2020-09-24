eea0195f4e786c517aaa74ef869266cf36228946
marcelbade <62931394+marcelbade@users.noreply.github.com>
2020-09-24 20:38:34 +0200

    added item repo, NO SQL BACKING FOR ITEMS YET

--- src/main/java/marcel/demonworld/armygenerator/repositories/ItemRepository.java
+++ src/main/java/marcel/demonworld/armygenerator/repositories/ItemRepository.java
@@ -1,6 +1,10 @@
 package marcel.demonworld.armygenerator.repositories;
 
-//@Repository
-public interface ItemRepository //extends JpaRepository<ItemCard, Integer>
-{
+import marcel.demonworld.armygenerator.dto.statCards.ItemCard;
+import marcel.demonworld.armygenerator.dto.statCards.UnitCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<ItemCard, Integer> {
 }
--- src/main/java/marcel/demonworld/armygenerator/services/ItemService.java
+++ src/main/java/marcel/demonworld/armygenerator/services/ItemService.java
@@ -1,45 +1,52 @@
 package marcel.demonworld.armygenerator.services;
 
-//@Service
+import marcel.demonworld.armygenerator.dto.statCards.ItemCard;
+import marcel.demonworld.armygenerator.repositories.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.*;
+
+@Service
 public class ItemService {
 
-//    @Autowired
-//    ItemRepository repo;
-//
-//    /**
-//     * returns all items of one faction, including generic!
-//     *
-//     * @param faction: army faction in the game
-//     * @return all items that belong to a faction
-//     */
-//    public List<ItemCard> returnAllItemsofFaction(String faction) {
-//        return repo.findAll().stream().filter(i -> i.getFaction().equals(faction)).collect(Collectors.toList());
-//    }
-//
-//
-//    /**
-//     *
-//     * @param unitType
-//     * @return
-//     */
-//    public List<ItemCard> returnAllItemforUnitType(String unitType) {
-//        return repo.findAll().stream().filter(i -> i.getUsedBy().equalsIgnoreCase(unitType)).collect(Collectors.toList());
-//
-//    }
-//
-//
-//    //TODO: you need to check for primary and secondary type, i.e., infantry and undead
-//
-//    /**
-//     *
-//     * @param faction
-//     * @param unitType
-//     * @return
-//     */
-//    public List<ItemCard> returnALlforUnitTypeandFaction(String faction, String unitType) {
-//            //TODO: FINISH THIS :)
-//        return repo.findAll().stream().filter(i -> i.getUsedBy() == unitType && i.getFaction().equals(faction)).collect(Collectors.toList());
-//    }
+    @Autowired
+    ItemRepository repo;
+
+  /**
+   * returns all items of one faction, including generic!
+    *
+    * @param faction: army faction in the game
+    * @return all items that belong to a faction
+    */
+   public List<ItemCard> returnAllItemsOfFaction(String faction) {
+       return repo.findAll().stream().filter(i -> i.getFaction().equals(faction)).collect(Collectors.toList());
+    }
+
+   /**
+     *
+     * @param unitType the unittype of the selected unit card
+     * @return A list of item cards filtered for the selected unit type
+     */
+    public List<ItemCard> returnAllItemsForUnitType(String unitType) {
+        return repo.findAll().stream().filter(i -> i.getUsedBy().equalsIgnoreCase(unitType)).collect(Collectors.toList());
+
+   }
+
+
+   //TODO: you need to check for primary and secondary type, i.e., infantry and undead
+
+    /**
+    *
+     * @param faction
+     * @param unitType
+    * @return
+     */
+    public List<ItemCard> returnAllItemsForUnitTypeandFaction(String faction, String unitType) {
+            //TODO: FINISH THIS :)
+        return repo.findAll().stream().filter(i -> i.getUsedBy() == unitType && i.getFaction().equals(faction)).collect(Collectors.toList());
+   }
 }
 
 
