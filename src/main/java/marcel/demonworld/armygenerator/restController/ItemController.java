58c2a4abf52974ee1ef43e8cf9a7a035b424b3f7
marcel2bade@gmail.com <k?M24#!!>
2020-12-15 22:54:42 +0100

    added rest endpoints...

---
+++ src/main/java/marcel/demonworld/armygenerator/restController/ItemController.java
@@ -0,0 +1,25 @@
+package marcel.demonworld.armygenerator.restController;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ItemController {
+//
+//    @Autowired
+//    ItemService service;
+//
+//    /**
+//     * Returns all unit cards of one faction
+//     *
+//     * @param faction;
+//     * @return all cards of one army
+//     */
+//    @GetMapping("/faction/{faction}")
+//    public List<ItemCard> getAllCardsofArmy(@PathVariable String faction, String type) {
+//        return  service.returnALlforUnitTypeandFaction( faction, type );
+//    }
+//
+//
+
+}
