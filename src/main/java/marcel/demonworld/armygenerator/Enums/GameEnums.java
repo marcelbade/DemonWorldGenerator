2e4136bd14b85df7028c502ed93864432dfe536a
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-12-03 09:12:52 +0100

    refromatted.

--- src/main/java/marcel/demonworld/armygenerator/Enums/GameEnums.java
+++ src/main/java/marcel/demonworld/armygenerator/Enums/GameEnums.java
@@ -3,7 +3,6 @@ package marcel.demonworld.armygenerator.Enums;
 public enum GameEnums {
     NONE,
     NO_ALLY,
-
     // generic items are marked with a "*" in the DB.
     GENERIC {
         @Override
