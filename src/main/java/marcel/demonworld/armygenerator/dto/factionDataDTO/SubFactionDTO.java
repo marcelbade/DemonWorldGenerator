b1cd26aa80f3fc24018e4be911b069aa940f3f88
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-05-15 13:21:02 +0200

    Added comments.

--- src/main/java/marcel/demonworld/armygenerator/dto/factionDataDTO/SubFactionDTO.java
+++ src/main/java/marcel/demonworld/armygenerator/dto/factionDataDTO/SubFactionDTO.java
@@ -9,8 +9,17 @@ import java.util.List;
 @Data
 public class SubFactionDTO {
 
+    // name of the subFaction
     private String name;
+    // some faction have several alternative army lists with different sub factions as options.
     private boolean alternativeListOption;
+    // true, if the sub faction is the selected option
     private boolean selectedAlternativeOption;
+    /**
+     * true, if all units have been blocked by validation logic, i.e., if none of the units in this sub faction can be
+     * selected by the player.
+     */
+    private boolean hasNoValidUnits;
+    // units that belong to this sub faction
     private List<UnitCard> units;
 }
