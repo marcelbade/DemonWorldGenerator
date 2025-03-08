87e73fb513794079956b23097aa45fe3421ee14e
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-03-08 22:30:06 +0100

    Refactored returnAll method.

--- src/main/java/marcel/demonworld/armygenerator/services/game/SelectArmyService.java
+++ src/main/java/marcel/demonworld/armygenerator/services/game/SelectArmyService.java
@@ -1,23 +1,34 @@
 package marcel.demonworld.armygenerator.services.game;
 
-import marcel.demonworld.armygenerator.dto.game.EntityDTOs.UnitCard;
+import lombok.AllArgsConstructor;
+import marcel.demonworld.armygenerator.dto.game.EntityDTOs.UnitCardDTO;
+import marcel.demonworld.armygenerator.entities.game.UnitCard;
+import marcel.demonworld.armygenerator.mapperImplementations.game.UnitCardDtoToUnitCardMapper;
 import marcel.demonworld.armygenerator.repositories.game.ArmyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 public class SelectArmyService {
 
     @Autowired
     ArmyRepository repo;
 
+    @Autowired
+    UnitCardDtoToUnitCardMapper mapper;
+
     /**
      * Method returns all units in the game.
+     *
      * @return List<UnitCard>
      */
-    public List<UnitCard> returnAll(){
-        return repo.findAll();
+    public List<UnitCardDTO> returnAll() { //
+        List<UnitCard> all = repo.findAll();
+
+        return all.stream().map(a -> mapper.UnitCardToUnitCardDto(a)).collect(Collectors.toList());
     }
 }
