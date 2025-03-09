1e19787a1915c0f105f1d41c858d2ead20b28290
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-03-09 16:13:58 +0100

    Added armyList service and repo.

--- src/main/java/marcel/demonworld/armygenerator/repositories/game/ArmyListRepository.java
+++ src/main/java/marcel/demonworld/armygenerator/repositories/game/ArmyListRepository.java
@@ -1,11 +1,18 @@
 package marcel.demonworld.armygenerator.repositories.game;
 
 
-import marcel.demonworld.armygenerator.dto.game.EntityDTOs.ArmyListDTO;
 import marcel.demonworld.armygenerator.entities.game.ArmyList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ArmyListRepository extends JpaRepository<ArmyList, Long> {
+
+    @Query("Select * from ")
+    List<ArmyList> findAllListsByUser(String userName);
+
+
 }
---
+++ src/main/java/marcel/demonworld/armygenerator/services/game/ArmyListService.java
@@ -0,0 +1,33 @@
+package marcel.demonworld.armygenerator.services.game;
+
+
+import lombok.AllArgsConstructor;
+import marcel.demonworld.armygenerator.dto.game.EntityDTOs.ArmyListDTO;
+import marcel.demonworld.armygenerator.entities.game.ArmyList;
+import marcel.demonworld.armygenerator.mappingInterfaces.game.ArmyListMapper;
+import marcel.demonworld.armygenerator.repositories.game.ArmyListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class ArmyListService {
+
+    @Autowired
+    ArmyListRepository repo;
+
+    @Autowired
+    ArmyListMapper mapper;
+
+
+    public List<ArmyListDTO> returnListsForUser(String userName) {
+
+        List<ArmyList> all = repo.findAllListsByUser(userName);
+
+        return all.stream().map(a -> mapper.entityToDTO(a)).collect(Collectors.toList());
+    }
+
+}
