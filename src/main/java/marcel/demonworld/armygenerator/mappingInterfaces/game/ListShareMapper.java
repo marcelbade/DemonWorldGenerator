4d59c67007a0697fdddfdf24593a43dd725743e8
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-03-10 19:22:41 +0100

    ArmyList: DTO appended to match entity and DB, mapper appended, custom query added.

--- src/main/java/marcel/demonworld/armygenerator/mappingInterfaces/game/ListShareMapper.java
+++
@@ -1,8 +0,0 @@
-package marcel.demonworld.armygenerator.mappingInterfaces.game;
-
-import marcel.demonworld.armygenerator.dto.game.EntityDTOs.ListShareDTO;
-import marcel.demonworld.armygenerator.entities.game.ListShare;
-
-public interface ListShareMapper {
-        ListShareDTO entityToDto(ListShare share);
-}
