5ae725cf9cdba7cd5630d2411d1e9d097bb277cf
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-03-11 15:30:58 +0100

    Added Methods to card services.

--- src/main/java/marcel/demonworld/armygenerator/services/game/ItemCardService.java
+++ src/main/java/marcel/demonworld/armygenerator/services/game/ItemCardService.java
@@ -1,14 +1,17 @@
 package marcel.demonworld.armygenerator.services.game;
 
 import lombok.AllArgsConstructor;
+import marcel.demonworld.armygenerator.Exceptions.AppException;
 import marcel.demonworld.armygenerator.dto.game.EntityDTOs.ItemCardDTO;
 import marcel.demonworld.armygenerator.entities.game.ItemCard;
 import marcel.demonworld.armygenerator.mapperImplementations.game.ItemCardMapperImplementation;
 import marcel.demonworld.armygenerator.repositories.game.ItemCardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -34,7 +37,20 @@ public class ItemCardService {
     }
 
     public void createNewItem(ItemCardDTO newItem) {
-        repo.save(mapper.DtoToEntity(newItem));
+
+        Optional<ItemCard> foundItem = repo.findByNameAndFaction(newItem.getItemName(), newItem.getFaction());
+
+        if (foundItem.isPresent()) {
+            throw new AppException("unit already exists for this faction!", HttpStatus.BAD_REQUEST);
+        } else {
+            repo.save(mapper.DtoToEntity(newItem));
+        }
+    }
+
+
+    public void updateItem(ItemCardDTO item) {
+
+        repo.save(mapper.DtoToEntity(item));
     }
 
 }
--- src/main/java/marcel/demonworld/armygenerator/services/game/UnitCardService.java
+++ src/main/java/marcel/demonworld/armygenerator/services/game/UnitCardService.java
@@ -1,14 +1,17 @@
 package marcel.demonworld.armygenerator.services.game;
 
 import lombok.AllArgsConstructor;
+import marcel.demonworld.armygenerator.Exceptions.AppException;
 import marcel.demonworld.armygenerator.dto.game.EntityDTOs.UnitCardDTO;
 import marcel.demonworld.armygenerator.entities.game.UnitCard;
 import marcel.demonworld.armygenerator.mapperImplementations.game.unitCardMapperImplementation;
 import marcel.demonworld.armygenerator.repositories.game.UnitCardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -32,8 +35,19 @@ public class UnitCardService {
         return all.stream().map(mapper::entityToDto).collect(Collectors.toList());
     }
 
+    public void updateUnitCard(UnitCardDTO unit) {
+        repo.save(mapper.dtoToEntity(unit));
+    }
+
     public void createNewUnit(UnitCardDTO newUnit) {
-        repo.save(mapper.dtoToEntity(newUnit));
+
+        Optional<UnitCard> foundUnit = repo.findByNameAndFaction(newUnit.getUnitName(), newUnit.getFaction());
+
+        if (foundUnit.isPresent()) {
+            throw new AppException("unit already exists for this faction!", HttpStatus.BAD_REQUEST);
+        } else {
+            repo.save(mapper.dtoToEntity(newUnit));
+        }
     }
 
 }
