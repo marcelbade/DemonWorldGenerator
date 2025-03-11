3e005e3054aaddc84535cda38c1f42e95b6bfa99
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-03-11 15:30:02 +0100

    Added custom queries to find cards by name and faction.

--- src/main/java/marcel/demonworld/armygenerator/repositories/game/ItemCardRepository.java
+++ src/main/java/marcel/demonworld/armygenerator/repositories/game/ItemCardRepository.java
@@ -2,8 +2,16 @@ package marcel.demonworld.armygenerator.repositories.game;
 
 import marcel.demonworld.armygenerator.entities.game.ItemCard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ItemCardRepository extends JpaRepository<ItemCard, Integer> {
+
+    @Query("SELECT ItemCard i FROM items i WHERE i.itemName = :passedName AND i.faction = :passedFaction")
+    Optional<ItemCard> findByNameAndFaction(@Param("passedName") String itemName, @Param("passedFaction") String faction);
+
 }
--- src/main/java/marcel/demonworld/armygenerator/repositories/game/UnitCardRepository.java
+++ src/main/java/marcel/demonworld/armygenerator/repositories/game/UnitCardRepository.java
@@ -2,9 +2,16 @@ package marcel.demonworld.armygenerator.repositories.game;
 
 import marcel.demonworld.armygenerator.entities.game.UnitCard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 
 @Repository
 public interface UnitCardRepository extends JpaRepository<UnitCard, Integer> {
+
+    @Query("SELECT UnitCard u FROM units u WHERE u.unitName = :passedName AND u.faction = :passedFaction")
+    Optional<UnitCard> findByNameAndFaction(@Param("passedName") String name, @Param("passedFaction") String faction);
 }
