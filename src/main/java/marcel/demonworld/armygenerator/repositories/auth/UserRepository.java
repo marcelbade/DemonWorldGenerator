837f24c7dd9998ef6d41dcd0864c66eef7597929
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-03-13 20:12:29 +0100

    Added missing Query to user repo.

--- src/main/java/marcel/demonworld/armygenerator/repositories/auth/UserRepository.java
+++ src/main/java/marcel/demonworld/armygenerator/repositories/auth/UserRepository.java
@@ -1,9 +1,10 @@
 package marcel.demonworld.armygenerator.repositories.auth;
 
 
-import marcel.demonworld.armygenerator.dto.auth.UserDTO;
 import marcel.demonworld.armygenerator.entities.auth.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,6 +12,8 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    Optional<User> findByUserName(String userName);
+
+    @Query("SELECT u FROM User u WHERE u.userName = :passedUserName")
+    Optional<User> findByUserName(@Param("passedUserName") String userName);
 
 }
