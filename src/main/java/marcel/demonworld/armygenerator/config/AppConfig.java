18b2618c137806c1177d0b5cba4b27acc788127d
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-03-14 18:55:13 +0100

    Added config for property sources.

---
+++ src/main/java/marcel/demonworld/armygenerator/config/AppConfig.java
@@ -0,0 +1,9 @@
+package marcel.demonworld.armygenerator.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class AppConfig {
+}
