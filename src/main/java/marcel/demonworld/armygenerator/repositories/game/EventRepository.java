336697a943b0a0db58f75aaa6f352ce7da7f4bc0
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-03-18 00:44:31 +0100

    Added query.

--- src/main/java/marcel/demonworld/armygenerator/repositories/game/EventRepository.java
+++ src/main/java/marcel/demonworld/armygenerator/repositories/game/EventRepository.java
@@ -6,13 +6,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface EventRepository extends JpaRepository<Event, Long> {
 
 
     @Query("SELECT e from Event e WHERE e.organizer.userName = :userName")
-    List<Event> findAllEventsForUser( @Param("userName") String userName);
+    List<Event> findAllEventsForUser(@Param("userName") String userName);
 
-
+    @Query("SELECT e from Event e WHERE e.eventName = :eventName")
+    Optional<Event> findEventByName(@Param("eventName") String eventName);
 }
